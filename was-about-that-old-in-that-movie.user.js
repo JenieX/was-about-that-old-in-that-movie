7a4609febe4ec3c58d31a867b20a1f0a1a66b8c8
FlowerForWar <102888858+FlowerForWar@users.noreply.github.com>
2022-05-21 03:38:07 +0300

    1.Add zodiac signs 2.Highlight dead actors

--- was-about-that-old-in-that-movie.user.js
+++ was-about-that-old-in-that-movie.user.js
@@ -2,7 +2,7 @@
 // @name           Was about that old in that movie
 // @namespace      https://github.com/FlowerForWar/was-about-that-old-in-that-movie
 // @description    IMDb movies - hovering actors avatars would show how old they were when that movie was released
-// @version        0.01
+// @version        0.02
 // @author         FlowrForWar
 // @include        /https:\/\/www\.imdb\.com\/title\/tt\d+\/($|\?.+)/
 // @grant          none
@@ -11,7 +11,7 @@
 // @license        MIT
 // ==/UserScript==
 
-// Gif showing an examle
+// Gif showing an example
 // https://raw.githubusercontent.com/FlowerForWar/was-about-that-old-in-that-movie/main/example.gif
 
 (async function() {
@@ -45,7 +45,7 @@
 		const a = document.createElement('a');
 		a.setAttribute('class', 'title-cast-item__eps-toggle');
 		const span = document.createElement('span');
-		span.innerHTML = 'Fetching data..';
+		span.innerHTML = 'Loading data..';
 		a.appendChild(span);
 		siblingElement.appendChild(a);
 
@@ -56,7 +56,8 @@
 		const [actorBirthDate, actorDeathDate] = actorDates;
 
 		if (!actorBirthDate) {
-			span.innerHTML = "Couldn't tell!";
+			span.style.setProperty('text-decoration-line', 'line-through', 'important');
+			span.innerHTML = 'No birth date available';
 			return;
 		}
 		const actorInfo = {
@@ -66,7 +67,9 @@
 			'age-at-death': !!actorDeathDate && Math.floor((new Date(actorDeathDate) - new Date(actorBirthDate)) / 31536000000),
 		};
 
-		span.innerHTML = `Was about ${actorInfo['age-when-the-movie-first-released']} years old`;
+		const [, month, day] = actorBirthDate.split('-').map(string => Number(string));
+		if (actorDeathDate) span.style.setProperty('color', 'crimson', 'important');
+		span.innerHTML = `Was about ${actorInfo['age-when-the-movie-first-released']} years old <br>(${getZodiacSign(month, day)})`;
 
 		if (actorDeathDate) {
 			a.setAttribute('title', `${actorInfo.name}, died at the age of ${actorInfo['age-at-death']}`);
@@ -75,5 +78,61 @@
 			a.setAttribute('title', `Now, ${actorInfo.name} is ${actorInfo['age-now']} years old`);
 			this.setAttribute('title', `Now, ${actorInfo.name} is ${actorInfo['age-now']} years old`);
 		}
+	}
+
+	function getZodiacSign(month, day) {
+		let sign;
+		// let sign_Symbol;
+		switch (!0) {
+			case (month == 3 && day >= 21) || (month === 4 && day <= 19):
+				sign = 'Aries';
+				// sign_Symbol = '♈︎';
+				break;
+			case (month == 4 && day >= 20) || (month === 5 && day <= 20):
+				sign = 'Taurus';
+				// sign_Symbol = '♉︎';
+				break;
+			case (month == 5 && day >= 21) || (month === 6 && day <= 20):
+				sign = 'Gemini';
+				// sign_Symbol = '♊︎';
+				break;
+			case (month == 6 && day >= 21) || (month === 7 && day <= 22):
+				sign = 'Cancer';
+				// sign_Symbol = '♋︎';
+				break;
+			case (month == 7 && day >= 23) || (month === 8 && day <= 22):
+				sign = 'Leo';
+				// sign_Symbol = '♌︎';
+				break;
+			case (month == 8 && day >= 23) || (month === 9 && day <= 22):
+				sign = 'Virgo';
+				// sign_Symbol = '♍︎';
+				break;
+			case (month == 9 && day >= 23) || (month === 10 && day <= 22):
+				sign = 'Libra';
+				// sign_Symbol = '♎︎';
+				break;
+			case (month == 10 && day >= 23) || (month === 11 && day <= 21):
+				sign = 'Scorpio';
+				// sign_Symbol = '♏︎';
+				break;
+			case (month == 11 && day >= 22) || (month === 12 && day <= 21):
+				sign = 'Sagittarius';
+				// sign_Symbol = '♐︎';
+				break;
+			case (month == 12 && day >= 22) || (month === 1 && day <= 19):
+				sign = 'Capricorn';
+				// sign_Symbol = '♑︎';
+				break;
+			case (month == 1 && day >= 20) || (month === 2 && day <= 18):
+				sign = 'Aquarius';
+				// sign_Symbol = '♒︎';
+				break;
+			case (month == 2 && day >= 19) || (month === 3 && day <= 20):
+				sign = 'Pisces';
+				// sign_Symbol = '♓︎';
+				break;
+		}
+		return sign;
 	}
 })();
